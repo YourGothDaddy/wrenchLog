5063bec639b059540539dd56b20cd6267e56f3e1
YourGothDaddy <alexanderorjustalex@gmail.com>
2026-05-29 14:05:53 +0300

    Initial commit

---
+++ src/main/java/com/wrenchlog/wrenchlog/WrenchLogApplication.java
@@ -0,0 +1,13 @@
+package com.wrenchlog.wrenchlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrenchLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WrenchLogApplication.class, args);
+    }
+
+}
